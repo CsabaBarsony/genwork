040bec32d5281349b577ae4834d1eafa992dbbd6
Csaba Barsony <v-cbarsony@hotels.com>
2016-11-22 16:59:14 +0100

    events, actions

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 let nodeMaxId = 1;
 let edgeMaxId = 1;
 class Node {
-    constructor(connections = []) {
+    constructor(actions, generation, connections = []) {
+        this.entry = actions.entry;
+        this.exit = actions.exit;
+        this.generation = generation;
         this.connections = connections.map((c) => {
-            if(!(c instanceof Node)) {
+            if(!(c.node instanceof Node)) {
                 throw new Error('Node constructor: connection is not a Node.');
             }
             return c;
@@ -19,26 +22,52 @@ class Node {
         }
     }
 }
-let n1 = new Node();
+let n1 = new Node({ entry: null, exit: null }, 0);
 let dna = [
     {
-        rounds: 2,
-        type: 1
+        rounds: 1,
+        type: 3,
+        event: 1,
+        entry: 1,
+        exit: null
     },
     {
         rounds: 1,
-        type: 2
+        type: 1,
+        event: 2,
+        entry: 2,
+        exit: 3
+    },
+    {
+        rounds: 1,
+        type: 2,
+        event: 3,
+        entry: null,
+        exit: 2
     }
 ];
+let generation = 0;
 let nodes = [n1];
 dna.forEach((word) => {
     for(let i = 0; i < word.rounds; i++) {
-        nodes.forEach((n) => {
+        generation++;
+        nodes.forEach((parent) => {
+            var connections = nodes.map((n) => {
+                return {
+                    node: n,
+                    event: word.event
+                };
+            });
             if(word.type === 1) {
-                nodes.push(new Node(nodes));
+                nodes.push(new Node({ entry: word.entry, exit: word.exit }, generation, connections));
             }
             else if(word.type === 2) {
-                nodes.push(new Node([n]));
+                nodes.push(new Node({ entry: word.entry, exit: word.exit }, generation, [{ node: parent, event: word.event }]));
+            }
+            else if(word.type === 3) {
+                var newNode = new Node({ entry: word.entry, exit: word.exit }, generation);
+                parent.addConnection({ node: newNode, event: word.event });
+                nodes.push(newNode);
             }
         });
     }
@@ -54,9 +83,9 @@ nodes.forEach((n) => {
     n.connections.forEach((c) => {
         graph.push({
             data: {
-                id: '' + n.id + ', ' + c.id,
+                id: '' + n.id + ', ' + c.node.id,
                 source: n.id,
-                target: c.id
+                target: c.node.id
             }
         });
     });
@@ -84,3 +113,8 @@ let cy = cytoscape({
         }
     ]
 });
+let finishStates = 0;
+nodes.forEach((n) => {
+    if(n.connections.length === 0) finishStates++;
+});
+console.log('finish state ratio: ' + ((1 / (finishStates / nodes.length)) / nodes.length));
